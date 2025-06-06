d13732cb8ef6d1dbaa2f3c90e189bec067dc3c8a
rk <r.kawata5155@gmail.com>
2025-06-06 14:43:41 +0900

    firebaseconfigを環境変数にした

--- composables/useFirebase.ts
+++ composables/useFirebase.ts
@@ -7,15 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD3hDCbTJQLWQQ__N6ev2dEATG_uytxaY4",
-  authDomain: "reunion-app-3e01a.firebaseapp.com",
-  projectId: "reunion-app-3e01a",
-  storageBucket: "reunion-app-3e01a.firebasestorage.app",
-  messagingSenderId: "567462110388",
-  appId: "1:567462110388:web:649759ff7b3003baf623e1",
-  measurementId: "G-17LVTRHY11"
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+getAnalytics(app);
