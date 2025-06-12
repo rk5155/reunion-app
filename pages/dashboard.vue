29fa7789c5cce1c4c94a1c2f2d8dceedf04d33e7
rk <r.kawata5155@gmail.com>
2025-06-12 15:15:58 +0900

    ダッシュボードにヘッダーを表示した

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,33 +1,38 @@
 <template>
-  <v-container>
-    <v-card>
-      <v-card-title class="text-h5 text-center font-weight-bold mb-4">
-        ダッシュボード
-      </v-card-title>
-      <v-card-text>
-        <div class="text-center">
-          <h2 v-if="hydrated && userEmail">
-            ようこそ、{{ user.email }} さん！
-          </h2>
-          <h2 v-else>ようこそ、ゲストさん！</h2>
-          <p>ここでは、イベントの管理やプロフィールの編集ができます。</p>
-        </div>
-        <v-btn color="deep-purple-accent-1" @click="handleLogout">
-          ログアウト
-        </v-btn>
-      </v-card-text>
-    </v-card>
-  </v-container>
+  <v-app>
+    <v-container>
+      <Header />
+      <!-- Headerコンポーネントを追加 -->
+      <v-card>
+        <v-card-title class="text-h5 text-center font-weight-bold mb-4">
+          ダッシュボード
+        </v-card-title>
+        <v-card-text>
+          <div class="text-center">
+            <h2 v-if="hydrated && userEmail">
+              ようこそ、{{ user.email }} さん！
+            </h2>
+            <h2 v-else>ようこそ、ゲストさん！</h2>
+            <p>ここでは、イベントの管理やプロフィールの編集ができます。</p>
+          </div>
+          <v-btn color="deep-purple-accent-1" @click="handleLogout">
+            ログアウト
+          </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </v-app>
 </template>
 
 <script lang="ts" setup>
-import { useAuthStore } from '@/stores/auth'; // ★ authストアをインポート
+import { useAuthStore } from '@/stores/auth';
 import { storeToRefs } from 'pinia';
 import { useRouter } from 'vue-router';
+import Header from '@/components/Header.vue'; // Headerコンポーネントをインポート
 
 const router = useRouter();
 const authStore = useAuthStore();
-const { user } = storeToRefs(authStore); // user をリアクティブに取り出す
+const { user } = storeToRefs(authStore);
 const userEmail = computed(() => user.value?.email || null);
 const hydrated = ref(false);
 
@@ -35,7 +40,6 @@ onMounted(() => {
   hydrated.value = true;
 });
 
-// ログアウト処理
 const handleLogout = async () => {
   try {
     await authStore.logout();
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,8 +1,16 @@
 // plugins/vuetify.ts
 import { createVuetify } from 'vuetify'
 import { defineNuxtPlugin } from '#app'
+import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify/styles';
+
+
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const vuetify = createVuetify()
+  const vuetify = createVuetify({
+    icons: {
+      defaultSet: 'mdi',
+    },
+  })
   nuxtApp.vueApp.use(vuetify)
 })
