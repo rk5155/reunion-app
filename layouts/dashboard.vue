eef0b1ec86af0bbbceac650401dbd8a8958ac0af
rk <r.kawata5155@gmail.com>
2025-06-16 10:13:35 +0900

    fix

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,10 +1,27 @@
 <template>
   <v-app>
     <auth-header />
-    <NuxtPage />
+    <div class="content">
+      <NuxtPage />
+    </div>
   </v-app>
 </template>
 
 <script lang="ts" setup>
 import AuthHeader from '@/components/auth/Header.vue';
 </script>
+
+<style scoped>
+auth-header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+  height: 64px;
+}
+
+.content {
+  margin-top: 64px;
+}
+</style>
