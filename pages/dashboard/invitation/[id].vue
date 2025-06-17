dae399cefc14ce133a836bdd17db5eada7371873
rk <r.kawata5155@gmail.com>
2025-06-17 20:16:13 +0900

    デザイン調整

--- pages/dashboard/invitation/[id].vue
+++ pages/dashboard/invitation/[id].vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="invitation-detail">
-    <v-card class="pa-4 semi-transparent-card">
+  <div class="invitation-detail text-center">
+    <v-card class="px-4 semi-transparent-card py-8 mb-4">
       <!-- カードの上に画像を表示 -->
       <v-img
         src="/images/invitation.jpg"
@@ -8,18 +8,31 @@
         class="event-image mb-4"
         contain
       ></v-img>
-      <h2 class="text-center text-h6 font-weight-bold mb-0 text-black">
+      <p class="text-center text-h6 font-weight-bold mb-0 text-black">
         {{ invitation.title }}
-      </h2>
+      </p>
       <v-card-text>
-        <p>開催日: {{ invitation.date }}</p>
-        <p>開始時間: {{ invitation.startTime }}</p>
-        <p>終了時間: {{ invitation.endTime }}</p>
-        <p>会場名: {{ invitation.venueName }}</p>
-        <p>会場住所: {{ invitation.venueAddress }}</p>
-        <p>締切日: {{ invitation.deadline }}</p>
-        <p>説明: {{ invitation.description }}</p>
-        <p>招待状ID: {{ invitationId }}</p>
+        <div class="mb-2 text-h7 font-weight-bold">{{ invitation.date }}</div>
+        <div class="mb-2 text-h7 font-weight-bold">
+          {{ invitation.startTime }} - {{ invitation.endTime }}
+        </div>
+        <div class="mb-2 text-h7 font-weight-bold">
+          {{ invitation.venueName }}
+        </div>
+        <div class="text-h7 font-weight-bold">
+          {{ invitation.venueAddress }}
+        </div>
+      </v-card-text>
+    </v-card>
+    <v-card class="px-4 semi-transparent-card py-8">
+      <v-card-text class="pa-0">
+        <div class="mb-2">{{ invitation.description }}</div>
+        <div class="mb-2 text-h7 font-weight-bold">
+          回答締切: {{ invitation.deadline }}
+        </div>
+        <div class="mb-2 text-h7 font-weight-bold">
+          招待状ID: {{ invitationId }}
+        </div>
       </v-card-text>
     </v-card>
     <v-btn color="primary" class="mt-4" @click="handleEdit">編集する</v-btn>
@@ -79,12 +92,15 @@ const handleBack = () => {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Kiwi+Maru&family=Zen+Antique&display=swap');
+
 .invitation-detail {
   background-image: url('/images/bg.png'); /* 背景画像を設定 */
   background-size: cover; /* 背景画像を画面全体にフィット */
   background-position: center; /* 背景画像を中央に配置 */
   min-height: 100vh; /* コンテンツが少ない場合でも画面全体を覆う */
   padding: 20px; /* コンテンツの余白を設定 */
+  font-family: 'Kiwi Maru', serif; /* フォントを適用 */
 }
 
 .event-image {
@@ -96,10 +112,12 @@ const handleBack = () => {
 h2 {
   margin-bottom: 20px;
   color: #fff; /* 背景に合わせて文字色を白に変更 */
+  font-family: 'Zen Antique', serif; /* フォントを適用 */
 }
 
 .semi-transparent-card {
   background-color: rgba(255, 255, 255, 0.8); /* 背景を半透明に設定 */
   border-radius: 10px; /* カードの角を丸くする */
+  font-family: 'Kiwi Maru', serif; /* フォントを適用 */
 }
 </style>
