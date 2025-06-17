c7ed74691466992c544d410bee5c3ac7284e7c32
rk <r.kawata5155@gmail.com>
2025-06-17 19:22:28 +0900

    招待状内容表示

--- pages/dashboard/invitation/[id].vue
+++ pages/dashboard/invitation/[id].vue
@@ -7,6 +7,12 @@
         >開催日: {{ invitation.date }}</v-card-subtitle
       >
       <v-card-text>
+        <p>開始時間: {{ invitation.startTime }}</p>
+        <p>終了時間: {{ invitation.endTime }}</p>
+        <p>会場名: {{ invitation.venueName }}</p>
+        <p>会場住所: {{ invitation.venueAddress }}</p>
+        <p>締切日: {{ invitation.deadline }}</p>
+        <p>説明: {{ invitation.description }}</p>
         <p>招待状ID: {{ invitationId }}</p>
       </v-card-text>
     </v-card>
@@ -14,25 +20,41 @@
     <v-btn color="secondary" class="mt-4" @click="handleBack">戻る</v-btn>
   </div>
 </template>
+
 <script lang="ts" setup>
 import { useRouter, useRoute } from 'vue-router';
 import { doc, getDoc } from 'firebase/firestore';
 import { useFirebase } from '@/composables/useFirebase';
 import { ref, onMounted } from 'vue';
+
 definePageMeta({ layout: 'dashboard' });
+
 const { db } = useFirebase();
 const router = useRouter();
 const route = useRoute();
 const invitationId = route.params.id;
-const invitation = ref({ title: '', date: '' });
+
+const invitation = ref({
+  title: '',
+  date: '',
+  startTime: '',
+  endTime: '',
+  venueName: '',
+  venueAddress: '',
+  deadline: '',
+  description: '',
+});
+
 onMounted(async () => {
   if (!invitationId) {
     alert('招待状IDが見つかりません');
     router.push('/dashboard');
     return;
   }
+
   const docRef = doc(db, 'invitations', invitationId);
   const docSnap = await getDoc(docRef);
+
   if (docSnap.exists()) {
     invitation.value = docSnap.data();
   } else {
