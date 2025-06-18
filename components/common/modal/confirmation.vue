1b9a9375f49385737dbac72d4a4bf7693732f6e3
rk <r.kawata5155@gmail.com>
2025-06-18 17:22:42 +0900

    fix

--- components/common/modal/confirmation.vue
+++ components/common/modal/confirmation.vue
@@ -4,20 +4,47 @@
     @update:model-value="(val) => emit('update:isVisible', val)"
     persistent
   >
-    <v-card>
-      <v-card-title class="text-h6">{{ title }}</v-card-title>
-      <v-card-text>
-        <p>{{ message }}</p>
-        <ul v-if="details">
-          <li v-for="(value, key) in details" :key="key">
-            <strong>{{ key }}:</strong> {{ value }}
-          </li>
-        </ul>
+    <v-card class="px-4 py-8">
+      <v-card-title class="text-h6 text-center mb-4 font-weight-bold pa-0">{{
+        title
+      }}</v-card-title>
+      <v-card-text class="pa-0 mb-6">
+        <p class="mb-4">{{ message }}</p>
+
+        <div class="mb-2 border-b py-2">
+          <p>{{ details.attendance }}</p>
+        </div>
+        <div class="mb-2 border-b py-2">
+          <p class="mb-2">氏名</p>
+          <p>{{ details.name }}</p>
+        </div>
+        <div class="mb-2 border-b py-2">
+          <p class="mb-2">メールアドレス</p>
+          <p>{{ details.email }}</p>
+        </div>
+        <div class="mb-2 border-b py-2">
+          <p class="mb-2">クラス</p>
+          <p>{{ details.className }}</p>
+        </div>
+        <div class="mb-2 border-b py-2">
+          <p class="mb-2">メッセージ</p>
+          <p>{{ details.message }}</p>
+        </div>
       </v-card-text>
-      <v-card-actions>
-        <v-btn color="green" @click="onConfirm">確認</v-btn>
-        <v-btn color="red" @click="onCancel">キャンセル</v-btn>
-      </v-card-actions>
+      <div class="d-flex">
+        <v-btn
+          color="cyan-darken-1"
+          @click="onConfirm"
+          class="w-50 font-weight-bold"
+          >送信</v-btn
+        >
+        <v-btn
+          color="grey-lighten-3"
+          @click="onCancel"
+          class="w-50 font-weight-bold"
+          >キャンセル</v-btn
+        >
+      </div>
     </v-card>
   </v-dialog>
 </template>
@@ -31,7 +58,7 @@ const props = defineProps({
   },
   message: {
     type: String,
-    default: '以下の内容で送信しますか？',
+    default: '以下の内容でよろしいですか？',
   },
   details: {
     type: Object,
