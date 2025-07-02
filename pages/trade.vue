dc9087ab76a60fa986825ea90b3a7c6482a7ede8
rk <r.kawata5155@gmail.com>
2025-07-02 14:22:00 +0900

    特定商取引法に基づく表記作成

---
+++ pages/trade.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-container>
+    <v-card class="py-6">
+      <v-card-title class="text-h5 text-center font-weight-bold mb-4">
+        特定商取引法に基づく表記（個人・同窓会代行サービス向け）
+      </v-card-title>
+      <v-card-text>
+        <dl>
+          <dt class="font-weight-bold mb-1">販売業者の名称</dt>
+          <dd class="mb-4">川田 隆稀（個人事業主）</dd>
+
+          <dt class="font-weight-bold mb-1">運営統括責任者</dt>
+          <dd class="mb-4">川田 隆稀</dd>
+
+          <dt class="font-weight-bold mb-1">所在地</dt>
+          <dd class="mb-4">※請求があった際には遅滞なく開示いたします。</dd>
+
+          <dt class="font-weight-bold mb-1">電話番号</dt>
+          <dd class="mb-4">※請求があった際には遅滞なく開示いたします。</dd>
+
+          <dt class="font-weight-bold mb-1">メールアドレス</dt>
+          <dd class="mb-4">blank.zone.free+reunion-app@gmail.com</dd>
+
+          <dt class="font-weight-bold mb-1">ホームページURL</dt>
+          <dd class="mb-4">
+            <a
+              href="https://reunion-app-new.web.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://reunion-app-new.web.app/
+            </a>
+          </dd>
+
+          <dt class="font-weight-bold mb-1">販売価格</dt>
+          <dd class="mb-4">各イベント案内ページに記載された金額（消費税込）</dd>
+
+          <dt class="font-weight-bold mb-1">サービスの提供時期</dt>
+          <dd class="mb-4">
+            開催当日にサービス提供<br />
+            ※詳細はイベントごとに記載された案内をご確認ください。
+          </dd>
+
+          <dt class="font-weight-bold mb-1">お支払い期限</dt>
+          <dd class="mb-4">
+            お支払い方法は、クレジットカード（Stripe決済）のみとなります。<br />
+            ご注文時に即時決済されます。
+          </dd>
+
+          <dt class="font-weight-bold mb-1">キャンセル・返金について</dt>
+          <dd class="mb-4">
+            お申込み後のキャンセルおよび返金は、いかなる理由においても一切お受けできません。<br />
+            やむを得ず参加できなくなった場合でも、ご返金はいたしかねますので、ご了承のうえお申込みをお願いいたします。<br /><br />
+          </dd>
+
+          <dt class="font-weight-bold mb-1">表示価格について</dt>
+          <dd class="mb-4">すべて税込価格です。</dd>
+        </dl>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
