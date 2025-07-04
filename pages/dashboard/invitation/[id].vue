5abfcbd2847753efa12c58b693a185bba75ed0be
rk <r.kawata5155@gmail.com>
2025-07-04 17:26:13 +0900

    fix

--- pages/dashboard/invitation/[id].vue
+++ pages/dashboard/invitation/[id].vue
@@ -264,7 +264,7 @@ const isDeadlinePassed = computed(() => {
 });
 
 const isCreator = computed(() => {
-  return invitation.value.creatorId === authStore.user?.uid; // 現在のユーザーIDと作成者IDを比較
+  return invitation.value.creatorId === authStore.user?.uid;
 });
 
 const updateCountdown = () => {
@@ -309,12 +309,11 @@ const handleBack = () => {
 };
 
 const saveReservation = async (formData: any) => {
-  // 1時間後
   const expireAt = Timestamp.fromDate(new Date(Date.now() + 60 * 60 * 1000));
 
   const reservationData = {
     ...formData,
-    invitationId: invitationId, // 招待状IDを追加
+    invitationId: invitationId,
     invitation: {
       title: invitation.value.title,
       date: invitation.value.date,
@@ -358,34 +357,34 @@ const handleFormSubmit = async (formData: Record<string, any>) => {
 @import url('https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&family=Kiwi+Maru&family=Zen+Antique&display=swap');
 
 .invitation-detail {
-  background-image: url('/images/bg.png'); /* 背景画像を設定 */
-  background-size: cover; /* 背景画像を画面全体にフィット */
-  background-position: center; /* 背景画像を中央に配置 */
-  min-height: 100vh; /* コンテンツが少ない場合でも画面全体を覆う */
-  padding: 20px; /* コンテンツの余白を設定 */
-  font-family: 'Zen Antique', serif !important; /* フォントを適用 */
+  background-image: url('/images/bg.png');
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh;
+  padding: 20px;
+  font-family: 'Zen Antique', serif !important;
 }
 
 .event-image {
   width: 100%;
   height: auto;
-  border-radius: 10px 10px 0 0; /* カードの上部に画像をフィットさせる */
+  border-radius: 10px 10px 0 0;
 }
 
 h2 {
   margin-bottom: 20px;
-  color: #fff; /* 背景に合わせて文字色を白に変更 */
+  color: #fff;
 }
 
 .semi-transparent-card {
-  background-color: rgba(255, 255, 255, 0.8); /* 背景を半透明に設定 */
+  background-color: rgba(255, 255, 255, 0.8);
 }
 .text-h2,
 .text-h3,
 .text-h4,
 .text-h5,
 .text-h6 {
-  font-family: 'Cinzel', serif !important; /* タイトルフォントを適用 */
+  font-family: 'Cinzel', serif !important;
 }
 
 .countdown-label {
@@ -420,12 +419,11 @@ h2 {
   right: 0;
 }
 
-/* 画面下部のボタンを調整 */
 .fixed-bottom {
-  background-color: rgba(255, 255, 255, 0.9); /* 半透明の背景 */
-  backdrop-filter: blur(10px); /* 背景をぼかす */
-  border-top: 1px solid #ddd; /* 上部に境界線 */
-  padding: 10px 0; /* 上下のパディング */
+  background-color: rgba(255, 255, 255, 0.9);
+  backdrop-filter: blur(10px);
+  border-top: 1px solid #ddd;
+  padding: 10px 0;
 }
 
 .sticky-bottom {
