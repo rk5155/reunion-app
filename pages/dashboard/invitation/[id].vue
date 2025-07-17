48f1f64681175b434506abb60c5ac4c3feca9163
rk <r.kawata5155@gmail.com>
2025-07-17 16:54:38 +0900

    google mapを表示した

--- pages/dashboard/invitation/[id].vue
+++ pages/dashboard/invitation/[id].vue
@@ -154,6 +154,23 @@
             </tr>
 
             <tr>
+              <td class="w-25">会場地図</td>
+              <td class="py-4">
+                <div class="map-container">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3232.1713175928653!2d139.62957457579728!3d35.893819622519324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018c10c85315335%3A0x549a9463abdad23c!2z44Oh44Or44Kr44OJIOODh-ODqyDjg5fjgqjjg4njg4g!5e0!3m2!1sja!2sjp!4v1752737687076!5m2!1sja!2sjp"
+                    width="100%"
+                    height="300"
+                    style="border: 0"
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                    class="rounded"
+                  ></iframe>
+                </div>
+              </td>
+            </tr>
+
+            <tr>
               <td class="w-25">会場住所</td>
               <td class="py-4">{{ invitation.venueAddress }}</td>
             </tr>
@@ -554,5 +571,20 @@ h2 {
   position: sticky;
   bottom: 0;
   z-index: 10;
+}
+
+.map-container {
+  overflow: hidden;
+  padding-top: 56.25%; /* 16:9 */
+  position: relative;
+  height: 0;
+}
+
+.map-container iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
